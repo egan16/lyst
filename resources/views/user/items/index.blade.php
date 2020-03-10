478e5c0a1883c4faa2ac54b38f13ff9aa10d5c4c
MichaelEgan <egan16@gmail.com>
2020-03-10 23:03:07 +0000

    Items index styled, list show styled

--- resources/views/user/items/index.blade.php
+++ resources/views/user/items/index.blade.php
@@ -6,54 +6,54 @@
 {{-- header and add button start --}}
 <header class="flex items-center justify-between flex-wrap mx-8 my-3">
     <div class="flex items-center flex-shrink-0">
-        <h1 class="w-16 text-4xl font-black text-mineshaft">Items</h1>
+        <h1 class="w-auto text-4xl font-black text-mineshaft">Items</h1>
     </div>
 </header>
 {{-- header and add button end --}}
 
+
 <div class="container my-12 mx-auto px-4 md:px-12">
-    <div class="flex flex-wrap sm:-mx-1 lg:-mx-4">
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
 
-        {{--Table start --}}
-        <table class="table-auto">
-            <thead>
-                <tr>
-                    <th class="px-4 py-2">Image</th>
-                    <th class="px-4 py-2">Title</th>
-                    <th class="px-4 py-2">Price</th>
-                    <th class="px-4 py-2">Item Code</th>
-                    <th class="px-4 py-2">URL</th>
-                    <th class="px-4 py-2">Date Added</th>
-                </tr>
-            </thead>
 
-            <tbody>
-                @if (count($items) ===0)
-                <p class="text-2xl">There are no items!</p>
-                @else
+        @if (count($items) ===0)
+        <p class="text-2xl text-mineshaft">There are no items!</p>
+        @else
 
-                @foreach ($items as $item)
-                <tr class="text-mineshaft">
-                    <td class="border px-4 py-2"><img class="object-contain h-48 w-full" src="{{ $item->image }}" /></td>
-                    <td class="border px-4 py-2">
+        @foreach ($items as $item)
+        <!-- Column -->
+        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
+            <!-- Article -->
+            <article class="overflow-hidden rounded-lg shadow-lg">
+                <img class="block h-48 m-4" src="{{ $item->image }}">
+                <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                    <h1 class="text-2xl truncate w-2/3">
                         <a class="no-underline hover:text-safetyorange text-mineshaft" href="{{ route('user.items.show', $item->id) }}">
+                            {{-- commented out possible for ItemListController --}}
+                            {{-- <a class="no-underline hover:text-safetyorange text-mineshaft" href="{{ route('user.lists.items.show', $list->id, $item->id) }}"> --}}
                             {{ $item->title }}
                         </a>
-                    </td>
-                    <td class="border px-4 py-2">{{ $item->price }}</td>
-                    <td class="border px-4 py-2">{{ $item->item_code }}</td>
-                    <td class="border px-4 py-2">
-                        <a class="hover:text-safetyorange my-2" href="{{ $item->url }}">{{ $item->url }}</a>
-                    </td>
+                    </h1>
+                    <p class="text-mineshaft text-sm">
+                        {{ $item->created_at->format('d-m-yy') }}
+                    </p>
 
-                    <td class="border px-4 py-2">{{ $item->created_at->format('d-m-yy') }}</td>
-                </tr>
-                @endforeach
-                @endif
-            </tbody>
-        </table>
+                </header>
 
-        {{-- Table end --}}
+                <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left mb-4 mr-4">
+                    <p class="text-sm leading-tight text-gray-600">{{ $item->price }}</p>
+                    <p class="text-sm leading-tight text-gray-600">{{ $item->item_code }}</p>
+                    <p class="text-sm leading-tight text-gray-600 truncate"><a class="hover:text-safetyorange my-2 w-1/2" href="{{ $item->url }}">{{ $item->url }}</a></p>
+
+                </div>
+
+            </article>
+            <!-- END Article -->
+        </div>
+        <!-- END Column -->
+        @endforeach
+        @endif
     </div>
 </div>
 @endsection
